39254bf28b40b8104213a7f10076a323d5d37beb
vatari <majestic13@gmail.com>
2022-10-19 20:15:35 +0300

    added home controller

---
+++ controllers/home.js
@@ -0,0 +1,40 @@
+const { isUser } = require("../middleware/guards");
+const { getItems, getUserAndItems, searchItem } = require("../services/item");
+const preload = require("../middleware/preload");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  const query = req.query.text;
+  let items = await getItems();
+
+  if (query) {
+    items = await searchItem(query);
+  }
+  res.render("home", { title: "Home Page", items });
+});
+
+router.get("/details", async (req, res) => {
+  const items = await getItems();
+  res.render("details", { title: "Details Page", items });
+});
+
+router.get("/details/:id", preload(true), async (req, res) => {
+  if (req.session.user) {
+    res.locals.item.hasUser = true;
+    res.locals.item.isOwner =
+      req.session.user?._id == res.locals.item.owner._id;
+  }
+
+  res.render("details", { title: "Details" });
+});
+
+router.get("/profile", preload(true), isUser(), async (req, res) => {
+  const user = await getUserAndItems(req.session.user._id);
+
+  let total = user.expenses.reduce((acc, x) => acc + x, 0);
+
+  res.render("profile", { title: "My Profile", user, total });
+});
+
+module.exports = router;
